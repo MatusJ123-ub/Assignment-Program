d8ac2c7553e1483f87c9bf7610d5bd7d6a7eab4f
Jose Matus <2021154532@ub.edu.bz>
2025-02-18 12:47:53 -0600

    just another commit

--- main.cpp
+++ main.cpp
@@ -43,6 +43,20 @@ int main() {
             // The rest of the menu implementation here
             //commiting to github
         }
+
+        /*
+         switch (choice) {
+            case 1: {
+                cout << "Enter task description: ";
+                getline(cin, description);
+                cout << "Enter priority (High/Medium/Low): ";
+                getline(cin, priority);
+                cout << "Enter due date: ";
+                getline(cin, dueDate);
+                taskList.addTask(description, priority, dueDate);
+                break;
+            }
+*/
     }
     return 0;
 }
