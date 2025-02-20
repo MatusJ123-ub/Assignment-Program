42f531a68c0d8c1c1d80987110068e118f657e2b
Jose Matus <2021154532@ub.edu.bz>
2025-02-19 21:52:56 -0600

    last minute fixes

--- task.cpp
+++ task.cpp
@@ -1,7 +1,8 @@
 #include "task.h"
 
 // Setup a new Task
-Task::Task(string desc, string prio, string date) {
+Task::Task(string desc, string prio, string date) 
+{
     description = desc;
     priority = prio;
     dueDate = date;
@@ -9,42 +10,52 @@ Task::Task(string desc, string prio, string date) {
     next = nullptr;
 }
 
-string Task::getDescription() {
+string Task::getDescription()
+ {
     return description; //Return the description of this task
 }
 
-string Task::getPriority() {
+string Task::getPriority() 
+{
     return priority; //Return the priority of this task
 }
 
-string Task::getDueDate() {
+string Task::getDueDate() 
+{
     return dueDate; //Return the due date of this task
 }
 
-bool Task::getIsComplete() {
+bool Task::getIsComplete() 
+{
     return isComplete; //Return whether the task is complete or not
 }
 
-Task* Task::getNext() {
+Task* Task::getNext() 
+{
     return next; //Return pointer to the next task in the list
 }
 
-void Task::setDescription(string desc) {
+void Task::setDescription(string desc)
+ {
     description = desc; //Set the description of this task to the provided value
 }
 
-void Task::setPriority(string prio) {
+void Task::setPriority(string prio) 
+{
     priority = prio; //Set the priority of this task to the provided value
 }
 
-void Task::setDueDate(string date) {
+void Task::setDueDate(string date) 
+{
     dueDate = date; //Set the due date of this task to the provided value
 }
 
-void Task::setIsComplete(bool complete) {
+void Task::setIsComplete(bool complete) 
+{
     isComplete = complete; //Set the completion status of this task to the provided value
 }
 
-void Task::setNext(Task* nextTask) {
+void Task::setNext(Task* nextTask) 
+{
     next = nextTask; //Set the next pointer to the provided task
 }
